51e87c16d62778fd365c53978d4fe63e39b043c3
shade88 <shade88@yandex.ru>
2013-02-16 20:42:26 +0600

    new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #vsdfsdf vcvbcv
+  # @return [Object]
+  def title
+    base_title2 = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title2
+    else
+      "#{base_title2} | #{@title}"
+    end
+  end
 end
